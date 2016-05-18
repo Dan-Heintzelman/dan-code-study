15365aa027321aee10af4bd3cec0ed838fe14e98
Dan Heintzelman <heintzelman@gmail.com>
2016-05-18 16:02:09 -0500

    add arraylist add function(untested)

--- data-structures-array-lists/array_list.rb
+++ data-structures-array-lists/array_list.rb
@@ -4,14 +4,31 @@ class ArrayList
 
   def initialize(size)
     @dynamic_array = FixedArray.new(size)
+    @capacity = 0
+    @index_count = size
   end
 
   def add(element)
-    length = @dynamic_array.size
-    new_array = FixedArray.new(length + 1)
-    length.times do |i|
-      new_array.set(i, @dynamic_array.get(0))
+    current_length = @dynamic_array.size
+    if @index_count <= @capacity
+      @capacity = current_length * 2
+      @new_array = FixedArray.new(@capacity)
+      expand_array
+      @index_count += 1
+      new_array.set(index_count, element)
+      @dynamic_array = new_array
+    else
+      @index_count += 1
+      @dynamic_array.set(index_count, element)
     end
+
+    def expand_array
+      @index_count.times do |i|
+        @new_array.set(i, @dynamic_array.get(i))
+      end
+    end
+
+
     @dynamic_array.set(length, element)
 
     # @dynamic_array.array.each_with_index do |el, i|
@@ -19,4 +36,14 @@ class ArrayList
     # end
   end
 
+  def modify_size(expansion_size)
+    if @dynamic_array.size >= @dynamic_array + expansion_size ==
+  end
+
 end
+
+before adding element to array, check to make sure if current size
+
+before doubling size of dynamic_array, check to see if its current size is greater than its size after ading 1
+
+when I double the array, take the previous length of the array, and keep adding elements to array until I hit that number and then double again
