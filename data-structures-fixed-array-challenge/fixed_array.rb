7d9331184d514552be3f2378d8992a3e8983f08b
Dan Heintzelman <heintzelman@gmail.com>
2016-05-18 11:42:04 -0500

    Add initial tests

--- data-structures-fixed-array-challenge/fixed_array.rb
+++ data-structures-fixed-array-challenge/fixed_array.rb
@@ -1,9 +1,14 @@
 class FixedArray
+  attr_accessor :array
 
   def initialize(size)
     @size = size
     @array = Array.new(size)
   end
+
+  # def length
+  #   @array.length
+  # end
 
   def get(index)
     raise ("Index out of bounds") unless index < @size && index >= 0
@@ -15,8 +20,19 @@ class FixedArray
     @array[index] = element
   end
 
+  def check_index(index)
 
+  end
 end
+
+# Interface
+# FixedArray.new(size) creates a new array @size elements
+
+# FixedArray.get(index) Get a value from the array at the specified index.
+
+blarb = FixedArray.new(10)
+# blarb(4) returns the element at this index
+# blarb(5, "thing"), should insert "thing" at position 5
 
 # Interface
 # FixedArray.new(size) creates a new array @size elements
--- data-structures-fixed-array-challenge/fixed_array_spec.rb
+++ data-structures-fixed-array-challenge/fixed_array_spec.rb
@@ -1,5 +1,23 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  let(:fixedarray) {fixedarray = FixedArray.new(4)}
+
+  it "Instantiates a new array with specified length." do
+    # fixedarray = FixedArray.new(4)
+    expect(fixedarray.array.length).to eq 4
+  end
+
+  it "Sets the element in arrray at specified index" do
+    # fixedarray = FixedArray.new(4)
+    fixedarray.set(0, "Element 0")
+    expect(fixedarray.array[0]).to eq "Element 0"
+  end
+
+  it "Returns the element from array at specified index." do
+    # fixedarray = FixedArray.new(4)
+    fixedarray.set(1, "meow")
+    expect(fixedarray.get(1)).to eq "meow"
+  end
+
 end
